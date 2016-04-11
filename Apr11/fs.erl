f6c6f6ce6196d89862ab320d520a24adcebb8965
pvlbzn <pvlbzn@gmail.com>
2016-04-11 12:21:39 +0300

    Erlang file server, Python minor automatization.

---
+++ Apr11/fs.erl
@@ -0,0 +1,15 @@
+-module(fs). 
+-export([start/1, loop/1]). 
+
+start(Dir) -> spawn(fs, loop, [Dir]).
+
+loop(Dir) ->
+    receive
+        {Client, list_dir} ->
+            Client ! {self(), file:list_dir(Dir)};
+        {Client, {get_file, File}} ->
+            Full = filename:join(Dir, File),
+            Client ! {self(), file:read_file(Full)}
+        end,
+        loop(Dir). 
+
---
+++ upd.py
@@ -0,0 +1,28 @@
+# Cron running task for project automatization.
+# TODO: Later on I want to add a some project analisys tools here.
+
+import datetime
+from pathlib import Path
+
+m = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+     'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+
+def get_name():
+    '''
+    Create folder name from the current date in format
+    3 first letters of the month + number of the day.
+    April 23 -> Apr23.
+    '''
+    now = datetime.datetime.now()
+    # -1 because tuple has a zero-index.
+    month = m[now.month - 1]
+    day = now.day
+    return month + str(day)
+
+def create_folder(name):
+    p = Path(name)
+    if not p.exists():
+        p.mkdir()
+
+if __name__ == '__main__':
+    create_folder(get_name())
