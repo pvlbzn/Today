ee8d4f5e4591ec112f8240b6eac4ee2f6b3c5e96
pvlbzn <pvlbzn@gmail.com>
2016-04-16 13:24:36 +0300

    Code sample.

---
+++ Apr16/Refreshing.java
@@ -0,0 +1,14 @@
+
+public class Refreshing {
+
+    public static void main(String[] args) {
+        Integer n1 = new Integer(5);
+        Integer n2 = new Integer(5);
+        System.out.println(isTheyEqual(n1, n2));
+    }
+
+    static boolean isTheyEqual(Integer n1, Integer n2) {
+        return n1 == n2;
+    }
+
+}
