a8e87b288acd70abb555db45154201e69f6d0df3
pvlbzn <pvlbzn@gmail.com>
2016-07-19 00:25:09 +0300

    Bash: counter

--- count.bash
+++ count.bash
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-# Number of folders equals to number of days.
-NDAY=$(ls -ld */ | wc -l)
-MDFILES=$(echo */* | grep .md | wc -w)
-WORDS=$(cat */*.md | wc -w)
 
-echo -e "Days total:\t$NDAY"
-echo -e ".md files:\t$MDFILES"
-echo -e ".md words:\t$WORDS"
+#
+# Information about number of days and how much already done.
+#
+NDAY=$( ls -ld */ | wc -l )
+DONE=$( (( ${NDAY:5}+1-1 )) )
+PERCENT=$( bc <<< "scale=2; "$DONE" / 3.65" )
+
+echo -e "days:\t\t${NDAY:5}"
+echo -e "done: \t\t$PERCENT%\n"
+
+
+#
+# Information about .md files, which are main used format. Code
+# files is not much relevant for stats.
+#
+MDFILES=$( echo */* | grep .md | wc -w )
+# This variable value is a bug-prone because of white space prefix
+# which should be cut for calculations.
+WORDS=$( cat */*.md | wc -w )
+
+echo -e ".md files:\t${MDFILES:5}"
+echo -e ".md words:\t${WORDS:3}\n"
+
+
+#
+# Information about averanges. Averange words per each .md file
+# and averange number of words per day.
+#
+AVGWORDS=$( (( ${WORDS:3} / ${MDFILES:5} )) )
+AVGPERDAY=$( (( ${WORDS:3} / ${NDAY:5} )) )
+
+echo -e "avg per .md:\t${AVGWORDS}"
+echo -e "avg per day:\t${AVGPERDAY}"
