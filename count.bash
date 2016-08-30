a3a16fb1747c7a74ccd3fd1c6f52aa9c85fe7380
pvlbzn <pvlbzn@gmail.com>
2016-08-30 10:22:43 +0200

    count.bash update

--- count.bash
+++ count.bash
@@ -2,18 +2,19 @@
 
 
 #
-# Information about number of days and how much already done.
+# Information about number of days and how much is already done.
 #
 NDAY=$( ls -ld */ | wc -l )
 DONE=$(( ${NDAY}+1-1 ))
 PERCENT=$( bc <<< "scale=2; "$DONE" / 3.65" )
 
+echo "Duration"
 echo -e "days: \t\t${NDAY:5}"
 echo -e "done: \t\t${PERCENT}%\n"
 
 
 #
-# Information about .md files, which are main used format. Code
+# Information about .md files, which is the main used format. Code
 # files is not much relevant for stats.
 #
 MDFILES=$( echo */* | grep .md | wc -w )
@@ -21,16 +22,18 @@ MDFILES=$( echo */* | grep .md | wc -w )
 # which should be cut for calculations.
 WORDS=$( cat */*.md | wc -w )
 
+echo "Count:"
 echo -e ".md files:\t${MDFILES:5}"
 echo -e ".md words:\t${WORDS:3}\n"
 
 
 #
 # Information about averanges. Averange words per each .md file
-# and averange number of words per day.
+# and an averange number of words per a day.
 #
 AVGWORDS=$(( ${WORDS} / ${MDFILES} ))
 AVGPERDAY=$(( ${WORDS} / ${NDAY} ))
 
+echo "Words per:"
 echo -e "avg per .md:\t${AVGWORDS}"
 echo -e "avg per day:\t${AVGPERDAY}"
